94b773d377df74256ddcd3899f2fcd782fcfdd49
ron51301 <you@example.com>
2022-02-10 16:07:58 +0800

    add local

--- app.py
+++ app.py
@@ -196,6 +196,7 @@ def getCarouselMessage(data):
 
 def getLocationConfirmMessage(title, latitude, longitude):
     message = dict()
+    
     return message
 
 
@@ -214,6 +215,11 @@ def getPlayStickerMessage():
 
 def getTaipei101LocationMessage():
     message = dict()
+    message["type"] = "location"
+    message["title"] = "台北101"
+    message["address"] = "110台北市信義區信義路五段7號"
+    message["latitude"] = "25.034056468449304"
+    message["longitude"] = "121.56466736984362"
     return message
 
 

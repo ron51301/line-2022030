a8f13e7b4f1cd4b89554e94c7f84717b1c008c2d
ron51301 <98503636+ron51301@users.noreply.github.com>
2022-03-04 16:21:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -168,28 +168,28 @@ def index():
         )
 
 
-    app = dash.Dash(__name__)
-    app.title = "Dash Pivottable"
-    server = app.server
+        app = dash.Dash(__name__)
+        app.title = "Dash Pivottable"
+        server = app.server
 
-    app.layout = html.Div(
-        [
-            Header("Dash Pivottable", app),
-            dash_pivottable.PivotTable(
-                id="table",
-                data=data,
-                cols=["Day of Week"],
-                colOrder="key_a_to_z",
-                rows=["Party Size"],
-                rowOrder="key_a_to_z",
-                rendererName="Grouped Column Chart",
-                aggregatorName="Average",
-                vals=["Total Bill"],
-                valueFilter={"Day of Week": {"Thursday": False}},
-            ),
-            html.Div(id="output"),
-        ]
-    )
+        app.layout = html.Div(
+            [
+                Header("Dash Pivottable", app),
+                dash_pivottable.PivotTable(
+                    id="table",
+                    data=data,
+                    cols=["Day of Week"],
+                    colOrder="key_a_to_z",
+                    rows=["Party Size"],
+                    rowOrder="key_a_to_z",
+                    rendererName="Grouped Column Chart",
+                    aggregatorName="Average",
+                    vals=["Total Bill"],
+                    valueFilter={"Day of Week": {"Thursday": False}},
+                ),
+                html.Div(id="output"),
+            ]
+        )
 
 
     @app.callback(
